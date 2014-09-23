b0ef02f935dff60e18b6c1860d7cf452e9700ade
Nicholas Furness <nfurness@nixta.com>
2014-09-23 19:49:14 -0400

    Lots of UI work, and remove actual extents, but just calculate numbers.

---
+++ res/ReadMe.md
@@ -0,0 +1,7 @@
+#TPK Size Estimator
+
+Visit at [http://nixta.github.io/extent-estimator](http://nixta.github.io/extent-estimator).
+
+Select zoom levels, and set the maps extent to see tile counts. If the count is less than 100,000 (the ArcGIS Online TPK generation limit), you can click `Estimate Size` to request the size of the TPK.
+
+If you zoom out too far, the tile count could exceed the actual number - the app uses the Map's extent, not the tile layer's max extent, and extrapolates where the former is greater than the latter.
--- res/app.js
+++ res/app.js
@@ -1,7 +1,8 @@
 var detailLevels = [13,14,15,16],
     midLevels = [10,11,12],
     overviewLevels = [5,6,7,8,9],
-    globalLevels = [1,2,3,4];
+    globalLevels = [1,2,3,4],
+    maxEstimateCount = 100000;
 
 var basemaps = {
   topo: {
@@ -10,13 +11,24 @@ var basemaps = {
   }
 };
 
+jQuery.fn.extend({
+    disable: function(state) {
+        return this.each(function() {
+            this.disabled = state;
+        });
+    }
+});
+
 $(document).ready(function () {
+  $.cookie.json = true;
+
   $('body').data('esri-gnip-translator', {
     clientId: 'ycUvqwhnMGffespX',
     jobs: {}
   });
 
   initAuthenticationState();
+  initializeUI();
   initializeMap();
 });
 
@@ -28,6 +40,8 @@ function __appState() {
 function initializeMap() {
   createMap('extentMap', function (theMap) {
     __appState().map = theMap;
+    theMap.getLayer(theMap.basemapLayerIds[0]).on('load', showEstimatedTileCount);
+
     require(['esri/layers/GraphicsLayer',
              'esri/symbols/SimpleLineSymbol', 
              'esri/symbols/SimpleFillSymbol', 
@@ -42,37 +56,73 @@ function initializeMap() {
       newLayer.renderer = new SimpleRenderer(tileFill);
       __appState().tileDisplayLayer = newLayer;
       theMap.addLayer(newLayer);
+      theMap.on('load', function () {
+        theMap.on('extent-change', showEstimatedTileCount);
+        showCurrentZoom();
+      });
+      theMap.on('zoom-end', showCurrentZoom);
     });
   });
 }
 
+function showCurrentZoom() {
+  $('#zoomLevels > button').removeClass('current-zoom');
+  $('#zoomLevels > button[data-zoom-level="' + __appState().map.getZoom() + '"]').addClass('current-zoom');
+}
+
 function estimateTPK() {
   var geomToEstimate = __appState().map.extent,
-      levelsToUse = detailLevels;
+      levelsToUse = getSelectedLevels();
 
-  getTilesForGeometry(geomToEstimate, levelsToUse)
-  .then(function (newGraphics) {
-    var tileDisplayLayer = __appState().tileDisplayLayer;
-    tileDisplayLayer.clear();
+  console.log('Estimating for levels:');
+  console.log(levelsToUse);
 
-    console.log('Got ' + newGraphics.length + ' graphics!');
+  // getTilesForGeometry(geomToEstimate, levelsToUse)
+  //   .then(function (newGraphics) {
+  //     var tileDisplayLayer = __appState().tileDisplayLayer;
+  //     tileDisplayLayer.clear();
 
-    if (newGraphics.length < 6000) {
-      for (var i=0; i<newGraphics.length; i++) {
-        tileDisplayLayer.add(newGraphics[i]);
-      }    
-    } else {
-      console.log('That\'s too many graphics to add. Won\'t bother');
-    }
-  });
+  //     console.log('Got ' + newGraphics.length + ' graphics!');
 
+  //     if (newGraphics.length < 6000) {
+  //       for (var i=0; i<newGraphics.length; i++) {
+  //         tileDisplayLayer.add(newGraphics[i]);
+  //       }    
+  //     } else {
+  //       console.log('That\'s too many graphics to add. Won\'t bother');
+  //     }
+  //   });
+
+  setTileSizeText(-1);
   requestEstimate(geomToEstimate, levelsToUse)
-  .then(function gotEstimate(estimate) {
-    console.log('Got estimate: ' + estimate.totalTilesToExport + ' tiles in ' + estimate.totalSize/1024/1024 + 'Mb');
-  }, function estimateFailed(err) {
-    console.error('Failed to get estimate:');
-    console.log(JSON.stringify(err, null, '  '));
-  });
+    .then(function gotEstimate(estimate) {
+      console.log('Got estimate: ' + estimate.totalTilesToExport + ' tiles in ' + estimate.totalSize/1024/1024 + 'Mb');
+      setTileSizeText(estimate.totalSize);
+    }, function estimateFailed(err) {
+      console.error('Failed to get estimate:');
+      console.log(JSON.stringify(err, null, '  '));
+      setTileSizeText('Failed to get estimate!');
+    });
+}
+
+function setTileSizeText(size) {
+  var str = '';
+  if ({}.toString.call(size) === '[object String]') {
+    str = size;
+  } else if (size === -1) {
+    str = 'Calculating size...';
+  } else {
+    str = (Math.round(1000*(size/1024/1024))/1000) + 'Mb';
+  }
+  if (str !== '') {
+    str = '(' + str + ')';
+  }
+  $('#tpkSizeDisplay').text(str);
+  $('#tpkSizeDisplay').removeClass('invalid');
+}
+
+function invalidateEstimate() {
+  $('#tpkSizeDisplay').addClass('invalid');
 }
 
 function requestEstimate(targetGeom, zoomLevels) {
@@ -153,7 +203,7 @@ function requestEstimate(targetGeom, zoomLevels) {
   return def;
 }
 
-function getTilesForGeometry(targetGeom, zoomLevels) {
+function getExtentsForGeometry(targetGeom, zoomLevels) {
   var def = new dojo.Deferred();
 
   var map = __appState().map,
@@ -161,6 +211,32 @@ function getTilesForGeometry(targetGeom, zoomLevels) {
       tileLayer = map.getLayer(basemapIds[0]),
       tileInfo = tileLayer.tileInfo;
 
+  return getExtentCountsForGeomWithTileInfo(targetGeom, zoomLevels, tileInfo);
+}
+
+function getExtentsForGeomWithTileInfo(targetGeom, zoomLevels, tileInfo) {
+  var def = new dojo.Deferred();
+
+  (function () {
+    var exts = getExtentsForGeomExtentWithTileInfo(targetGeom.getExtent(), zoomLevels, tileInfo, false);
+    def.resolve(exts);
+  })();
+
+  return def;
+}
+
+function getExtentCountsForGeomWithTileInfo(targetGeom, zoomLevels, tileInfo) {
+  var def = new dojo.Deferred();
+
+  (function () {
+    var exts = getExtentsForGeomExtentWithTileInfo(targetGeom.getExtent(), zoomLevels, tileInfo, true);
+    def.resolve(exts);
+  })();
+
+  return def;
+}
+
+function getExtentsForGeomExtentWithTileInfo(extent, zoomLevels, tileInfo, countOnly) {
   var selectedLODs = [];
   for (var i=0; i<tileInfo.lods.length; i++) {
     if (zoomLevels.indexOf(tileInfo.lods[i].level) !== -1) {
@@ -168,54 +244,132 @@ function getTilesForGeometry(targetGeom, zoomLevels) {
     }
   }
 
-  if (selectedLODs.length === 0) {
-    throw new Error('Could not find current LOD for ' + zoomLevels);
-  }
+  var tilesByZoomLevel = {};
+  
+  for (var lodIndex=0; lodIndex < selectedLODs.length; lodIndex++) {
+    var currentLOD = selectedLODs[lodIndex];
 
-  var extent = targetGeom.getExtent();
-  console.log(JSON.stringify(extent.toJson()));
-  console.log(zoomLevels);
+    var tileWidth = tileInfo.width * currentLOD.resolution,
+        tileHeight = tileInfo.height * currentLOD.resolution;
 
-  require(['esri/geometry/Extent', 'esri/graphic'], function (Extent, Graphic) {
-    var newGraphics = [];
+    var minCol = Math.floor((extent.xmin - tileInfo.origin.x) / tileWidth),
+        minRow = Math.floor(-(extent.ymax - tileInfo.origin.y) / tileHeight),
+        maxCol = Math.ceil((extent.xmax - tileInfo.origin.x) / tileWidth),
+        maxRow = Math.ceil(-(extent.ymin - tileInfo.origin.y) / tileHeight);
 
-    for (var lodIndex=0; lodIndex < selectedLODs.length; lodIndex++) {
-      var currentLOD = selectedLODs[lodIndex];
+    var extents = [];
 
-      console.log('Calculating for LOD ' + currentLOD.level);
-
-      var tileWidth = tileInfo.width * currentLOD.resolution,
-          tileHeight = tileInfo.height * currentLOD.resolution;
-
-      var minCol = Math.floor((extent.xmin - tileInfo.origin.x) / tileWidth),
-          minRow = Math.floor(-(extent.ymax - tileInfo.origin.y) / tileHeight),
-          maxCol = Math.ceil((extent.xmax - tileInfo.origin.x) / tileWidth),
-          maxRow = Math.ceil(-(extent.ymin - tileInfo.origin.y) / tileHeight);
-
-      var count = 0;
-
+    if (!countOnly) {
       for (var c=minCol; c < maxCol; c++) {
         for (var r=minRow; r < maxRow; r++) {
-          var tileExtent = new Extent(tileInfo.origin.x + (c * tileWidth), 
-            tileInfo.origin.y - ((r+1) * tileHeight),
-            tileInfo.origin.x + ((c+1) * tileWidth), 
-            tileInfo.origin.y - (r * tileHeight),
-            extent.spatialReference);
-          count++;
-          var tileGraphic = new Graphic(tileExtent, undefined, {
+          extents.push({
+            xmin: tileInfo.origin.x + (c * tileWidth), 
+            ymin: tileInfo.origin.y - ((r+1) * tileHeight),
+            xmax: tileInfo.origin.x + ((c+1) * tileWidth), 
+            ymax: tileInfo.origin.y - (r * tileHeight),
             row: r,
-            column: c,
-            zoom: currentLOD.level
+            column: c
           });
-          newGraphics.push(tileGraphic);
         }
       }
-
-      console.log('Create ' + count + ' graphics for zoom level ' + currentLOD.level);
+    } else {
+      extents = (maxCol-minCol-1) * (maxRow-minRow-1); 
     }
+    tilesByZoomLevel[currentLOD.level] = extents;
+  }
 
-    def.resolve(newGraphics);
+  return tilesByZoomLevel;
+}
+
+
+function getTilesForGeometry(targetGeom, zoomLevels) {
+  var def = new dojo.Deferred();
+
+  require(['esri/geometry/Extent', 'esri/graphic'], function (Extent, Graphic) {
+    getExtentsForGeometry(targetGeom, zoomLevels)
+      .then(function (tileExtents) {
+        var newGraphics = [],
+            count = 0;
+          
+        for (var zoomLevel in tileExtents) {
+          var extentsForZoom = tileExtents[zoomLevel];
+          console.log(extentsForZoom.length + ' tiles for zoom level ' + zoomLevel);
+          count += extentsForZoom.length;
+
+          for (var i=0; i<extentsForZoom.length; i++) {
+            var e = extentsForZoom[i],
+                tileExtent = new Extent(e.xmin, e.ymin, e.xmax, e.ymax, targetGeom.spatialReference),
+                tileGraphic = new Graphic(tileExtent, undefined, {
+                  row: e.row,
+                  column: e.column,
+                  zoom: zoomLevel
+                });
+            newGraphics.push(tileGraphic);
+          }
+        }
+
+        console.log(count + ' total tiles.');
+
+        def.resolve(newGraphics);
+      });
   });
 
   return def;
+}
+
+function getSelectedLevels() {
+  return $.makeArray($('#zoomLevels button.active').map(function(i, v) { 
+    return parseInt($(v).attr('data-zoom-level'));
+  }));
+}
+
+function initializeUI() {
+  $('#zoomLevels button').on('click', function () {
+    saveButtons();
+    invalidateEstimate();
+  });
+  $('#zoomLevels button').tooltip({
+    title: function() {
+      var zl = $(this).attr('data-tile-count'),
+          p = parseInt(zl) === 1?'':'s';
+      return zl + ' tile' + p;
+    }
+  });
+  $('#estimateButton').tooltip();
+  var savedButtons = $.cookie('selectedLevels');
+  if (savedButtons !== undefined) {
+    for (var i=0; i<savedButtons.length; i++) {
+      $('#zoomLevels button[data-zoom-level="' + savedButtons[i] + '"]').addClass('active');
+    }
+  }
+}
+
+function showEstimatedTileCount() {
+  var map = __appState().map;
+  var geomToEstimate = map.extent;
+  var selected = getSelectedLevels();
+  getExtentsForGeometry(geomToEstimate, selected)
+    .then(function (tileExtents) {
+      var count = 0;
+      $('#zoomLevels button').attr('data-tile-count', 0);
+
+      for (var zoomLevel in tileExtents) {
+        var tileCountForZoomLevel = tileExtents[zoomLevel];
+        $('#zoomLevels button[data-zoom-level="' + zoomLevel + '"').attr('data-tile-count', tileCountForZoomLevel);
+        count += tileCountForZoomLevel;
+      }
+      
+      var countStr = (''+count).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      $('#tileCountDisplay').text(countStr + ' tile' + (count!==1?'s':''));
+      $('#estimateButton').disable(count > maxEstimateCount);
+    });
+}
+
+function saveButtons() {
+  window.setTimeout(function () {
+    var selected = getSelectedLevels();
+    $.cookie('selectedLevels', selected, {expires: 365});
+    showEstimatedTileCount();
+  }, 100);
+  $(this).blur();
 }
